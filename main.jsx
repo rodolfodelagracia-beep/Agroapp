fbff8a6cf7cc9125fc3c252c1a0fd71a1201bd06
rodolfodelagracia-beep <rodolfo.delagracia@gmail.com>
2026-03-09 07:57:32 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,14 @@
+<svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <linearGradient id="paint0" x1="16" y1="16" x2="112" y2="112" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#2D7C45"/>
+      <stop offset="1" stop-color="#97C46C"/>
+    </linearGradient>
+  </defs>
+  <rect x="8" y="8" width="112" height="112" rx="28" fill="url(#paint0)"/>
+  <path d="M37 79C47 76 55 67 58 57C46 58 38 65 34 76C33 79 34 80 37 79Z" fill="white" fill-opacity="0.96"/>
+  <path d="M58 57C67 59 75 67 78 77C68 79 60 75 54 66C52 63 54 57 58 57Z" fill="white" fill-opacity="0.88"/>
+  <path d="M59 48V87" stroke="white" stroke-width="6" stroke-linecap="round"/>
+  <circle cx="83" cy="79" r="11" stroke="white" stroke-width="6"/>
+  <circle cx="48" cy="86" r="7" stroke="white" stroke-width="5"/>
+</svg>
